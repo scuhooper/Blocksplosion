2f2fef3cfc2f0a47f88616a28a33c7dadf073be6
scuhooper <keeling18@marshall.edu>
2017-03-20 03:34:33 -0400

    Blocks
    Added colliders to blocks.
    Added text for amount of hits left to blocks.
    Added script to change color of blocks every second to a random color.

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine( TimerToChangeColor() );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	IEnumerator TimerToChangeColor()
+	{
+		while ( true )
+		{
+			GetComponent<SpriteRenderer>().color = Random.ColorHSV( 0.2f, 1, 0.2f, 1, 0.2f, 1, 1, 1 );
+			yield return new WaitForSeconds( 1 );
+		}
+	}
+}
